1a656630f493c32fcafd40a8fdd7311ccb1e480b
Dibss <ludovicad01@gmail.com>
2022-08-06 19:07:44 +0200

    splitted code in two js files

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,18 +1,10 @@
-import "./css/ExpenseItem.css";
-import "./css/ExpenseDate.css";
+import ExpenseDate from "./ExpenseDate/ExpenseDate";
+import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', {month: 'long'});
-  const year = props.date.toLocaleString('en-US', {day: '2-digit'});
-  const day = props.date.getFullYear();
-
   return (
     <div className="expense-item">
-      <div className="expense-date">
-        <div className="expense-date__month">{month}</div>
-        <div className="expense-date__year">{year}</div>
-        <div className="expense-date__day">{day}</div>
-      </div>
+      <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
