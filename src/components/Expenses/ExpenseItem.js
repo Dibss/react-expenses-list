30f53256892ac8f9af667e38759afbe7e48bff93
Dibss <ludovicad01@gmail.com>
2022-08-08 21:53:56 +0200

    moved at the bottom extra code

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,29 +1,31 @@
-import React, {useState} from "react";
-
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 import "./css/ExpenseItem.css";
 
 const ExpenseItem = props => {
-
-  // firstValue = current state value, secondValue = function to update the first value
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title)
-  }
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change title</button>
     </Card>
   );
 }
 
 export default ExpenseItem;
+
+
+// EXTRA CODE TO OPTIONALLY INIT TITLE CHANGE
+// import React, {useState} from "react";
+
+// firstValue = current state value, secondValue = function to update the first value
+// const [title, setTitle] = useState(props.title);
+
+// const clickHandler = () => {
+//   setTitle('Updated!');
+//   console.log(title)
+// }
+
+//<button onClick={clickHandler}>Change title</button>
