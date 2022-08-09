6ba03a4cfcd8f91dccb0b8c2e632b0526157934c
Dibss <ludovicad01@gmail.com>
2022-08-09 16:41:32 +0200

    managed opening and closing the form section

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,21 +1,34 @@
+import React, { useState } from "react";
+
 import ExpenseForm from "./ExpenseForm";
 import "./css/NewExpense.css";
 
 const NewExpense = (props) => {
-  const saveExpenseDataHandler = enteredExpenseData => {
+  const [isAdding, setIsAdding] = useState(false);
+
+  const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString(),
     };
-
     props.onAddExpense(expenseData);
-  }
+    setIsAdding(false);
+  };
+
+  const addHandler = () => {
+    setIsAdding(true);
+  };
+
+  const closeClickHandler = () => {
+    setIsAdding(false);
+  };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+      {!isAdding && <button type="button" onClick={addHandler}>Add new expense</button>}
+      {isAdding && <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} closeForm={closeClickHandler}/>}
     </div>
   )
-}
+};
 
 export default NewExpense;
